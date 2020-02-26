240fffc787572ce668d3533f5d480306044a82c8
nakatest1234 <ambient2step@gmail.com>
2020-02-27 08:25:38 +0900

    for var weight saving

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,25 @@
 import boto3
 import time
 import os
+from datetime import datetime, timedelta, timezone
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def myTZ():
+	return int(os.environ.get('myTZ', 9))
+
+def datetime_jst(timestamp):
+	return datetime.fromtimestamp(timestamp/1000).astimezone(timezone(timedelta(hours=myTZ()))).strftime("%Y/%m/%d %H:%M:%S.%f%z")
 
 def day2timestamp(days):
 	return int((time.time() - (days * 24 * 3600))*1000) # ms
+
+def lambda_need(v, logGroupName):
+	return {
+		'logGroupName': logGroupName,
+		'logStreamName': v['logStreamName'],
+		'lastEvent': datetime_jst(v['lastEventTimestamp'])
+	}
 
 def lambda_handler(event, context):
 	# 対象ログストリームのlastEventTimestampの対象フィルタ検索の日数を遅らせる
@@ -16,9 +32,9 @@ def lambda_handler(event, context):
 	log_groups = list(filter(lambda x: 'retentionInDays' in x, client.describe_log_groups()['logGroups']))
 
 	# 削除arn
-	delete_arns = []
+	delete_targets = []
 
-	# ロググループを回す
+	# 削除対象を探す
 	for group in log_groups:
 		logGroupName = group['logGroupName']
 		log_streams = client.describe_log_streams(
@@ -30,18 +46,18 @@ def lambda_handler(event, context):
 		# 指定日数多め + サイズ0でフィルタ
 		targetTimestamp = day2timestamp(group['retentionInDays']+delay_days)
 		tmp = list(filter(lambda x: x['storedBytes']==0 and 'lastEventTimestamp'in x and x['lastEventTimestamp']<targetTimestamp, log_streams))
+		tmp2 = list(map(lambda v, logGroupName=logGroupName: lambda_need(v, logGroupName), tmp))
+		delete_targets.extend(tmp2)
 
-		for logStream in tmp:
-			# 削除対象
-			delete_arns.append(logStream['arn'])
-
-			if (dryrun==0):
-				client.delete_log_stream(
-					logGroupName=logGroupName,
-					logStreamName=logStream['logStreamName'],
-				)
+	# 削除実行
+	if (dryrun==0):
+		for target in delete_targets:
+			client.delete_log_stream(
+				logGroupName=target['logGroupName'],
+				logStreamName=target['logStreamName'],
+			)
 
 	return {
 		'dryrun': dryrun,
-		'deleted':delete_arns,
+		'delete_targets': delete_targets,
 	}
